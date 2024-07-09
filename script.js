3926e43927af640aa21bc8351d43a3250b342a95
Freeman's Freedom <freemansfreedom@gmail.com>
2024-07-09 07:28:09 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const input = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+
+
+
+const convert = () => {
+    output.innerText = input.value
+}
+
+// When Button or enter in textbox pressed
+//
+
+convert
